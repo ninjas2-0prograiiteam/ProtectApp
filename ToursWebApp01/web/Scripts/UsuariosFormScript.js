06446e91f7b0bb42d2dac9515cdfb6eeb1aed535
Herilen <helen.9.dic@gmail.com>
2019-04-18 00:48:23 -0600

    validacion de usuarios

---
+++ ToursWebApp01/web/Scripts/UsuariosFormScript.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+    $("#myform").validate({
+        rules:{
+            nombre: "required",
+            apellido: "required",
+            edad: {
+                required: true,
+                min: 18
+            },
+            dui: {
+                required: true,
+                min:9,
+                max:9
+            },
+            nit:{
+                required: true,
+                min:14,
+                max:14   
+            },
+            
+            email: "required",
+            user: "required",
+            contra : "required",
+            contracon: "required",
+            departamento: "required"
+        }
+        });
+$(document).ready(function ValidateEmail(email) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  {
+    return (true);
+  }
+    alert("Has ingresado un email invalido");
+    return (false);
+});
+
+$(document).ready(function allLetter(name)
+  {
+   var letters = /^[A-Za-z]+$/;
+   if(name.value.match(letters))
+     {
+      return true;
+     }
+   else
+     {
+     alert("Por favor ingresa un nombre valido");
+     return false;
+     }
+  });
+  
+  $(document).ready(function allLetter(apellido)
+  {
+   var letters = /^[A-Za-z]+$/;
+   if(apellido.value.match(letters))
+     {
+      return true;
+     }
+   else
+     {
+     alert("Por favor ingresa un apellido valido");
+     return false;
+     }
+  });
+  
+  $(document).ready(function ValidateSamePassword(contracon)
+  {
+      var contra = contra;
+   if(contracon.value.match(contra))
+     {
+      return true;
+     }
+   else
+     {
+     alert("Las contraseñas no coinciden");
+     return false;
+     }
+  });
+  
+$(document).ready(function CheckPassword(contra) 
+{ 
+var passw=  /^[A-Za-z]\w{7,14}$/;
+if(contra.value.match(passw)) 
+{ 
+return true;
+}
+else
+{ 
+alert("Ingrese una contraseña valida");
+return false;
+}
+});
+        });
+
