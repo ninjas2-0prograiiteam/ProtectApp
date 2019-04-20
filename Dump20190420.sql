d553ba19a6044a4dc9b11a78e79e60e79d329231
Herilen <helen.9.dic@gmail.com>
2019-04-20 00:13:49 -0600

    Base de datos actualizada

--- Dump20190420.sql
+++ Dump20190420.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `tourdatabase` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `tourdatabase`;
 -- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
 --
 -- Host: localhost    Database: tourdatabase
@@ -54,7 +52,7 @@ CREATE TABLE `categoria` (
   `tipo` varchar(45) NOT NULL,
   `descripcion` varchar(205) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +61,7 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'Cutural','Relacionado con la cultura de El Salvador'),(2,'Recreativo','Relacionado con el entertenimiento recreativo personal'),(3,'Grupos Ideológicos','Relacionado con grupos que comparten una ideología como LGBT, feministas, socialistas, entre otros.'),(4,'Deportivo','Relacionado con el deporte'),(5,'Rural','Realizados en las zonas rurales El Salvador'),(6,'Urbano','Realizado en las zonas urbanas de El Salvador'),(7,'De Aventura','Relacionado con la exploración de lugares, caminatas, canopi, escaladas, entre otros eventos'),(8,'De Negocios','Eventos para grupos de empresas'),(9,'Ecológico','Para ayudar a mejorar o limpiar zonas de El Salvador'),(10,'Religioso','Para personas que comparten una religión'),(11,'Gastronómico','Relacionado con la comida típica de El Salvador');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,12 +125,12 @@ CREATE TABLE `empresa` (
   `nombre` varchar(300) NOT NULL,
   `email` varchar(300) NOT NULL,
   `telefono` int(11) NOT NULL,
-  `nit` int(11) NOT NULL,
+  `nit` varchar(45) NOT NULL,
   `username` varchar(85) NOT NULL,
   `password` varchar(45) NOT NULL,
   `nodecuenta` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,6 +139,7 @@ CREATE TABLE `empresa` (
 
 LOCK TABLES `empresa` WRITE;
 /*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+INSERT INTO `empresa` VALUES (7,'Helen','helen.9.dic@gmail.com',79399834,'253754','4524524','45345',5452),(8,'Hola','helen.9.dic',79399834,'12365478963214','20171564','12345',12368515);
 /*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +161,7 @@ CREATE TABLE `evento` (
   `puntodeencuentro` varchar(500) NOT NULL,
   `precio` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -170,6 +170,7 @@ CREATE TABLE `evento` (
 
 LOCK TABLES `evento` WRITE;
 /*!40000 ALTER TABLE `evento` DISABLE KEYS */;
+INSERT INTO `evento` VALUES (2,1,1,11,'17:13','2019-04-27','16:12','Gasolinera Puma',4),(3,2,2,0,'7','7757','7','757',3),(5,4,2,2,'9','7757','7','757',15),(6,4,0,0,'9','156465','7','757',5),(7,4,2,4,'10','hoy','7','aqui',1),(8,4,2,4,'8','2022-03-04','03:03','hola',14),(9,8,6,7,'12:10','2019-12-09','19:16','Parque',20);
 /*!40000 ALTER TABLE `evento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,7 +218,7 @@ CREATE TABLE `usuarios` (
   `password` varchar(45) NOT NULL,
   `iddepartamento` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -226,7 +227,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (10,'Helen','Fuentes',21,954639873,'helen.9.dic@gmail.com','Helen Fuentes','12345',3),(11,'Helen','Fuentes',19,205698741,'helen.9.dic@gmail.com','Helen Fuentes','12345',1);
+INSERT INTO `usuarios` VALUES (10,'Helen','Fuentes',21,954639873,'helen.9.dic@gmail.com','Helen Fuentes','12345',14),(11,'Helen','Fuentes',19,205698741,'helen.9.dic@gmail.com','Helen Fuentes','12345',1),(14,'Marta','Sandoval',48,424894149,'helen.9.dic@gmail.com','Marty','123456',12);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -247,4 +248,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-18 22:09:26
+-- Dump completed on 2019-04-20  0:12:48
